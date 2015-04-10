f91600d393594db542e45558423778bcc4b96eda
balaji.sj <sjbalajimdu@gmail.com>
2015-04-10 23:21:01 +0530

    js file modified

--- facebookConnectPlugin.js
+++ facebookConnectPlugin.js
@@ -13,134 +13,9 @@ if (cordova.platformId == "browser") {
 
     var facebookConnectPlugin = {
 
-        getLoginStatus: function (s, f) {
-            // Try will catch errors when SDK has not been init
-            try {
-                FB.getLoginStatus(function (response) {
-                    s(response);
-                });
-            } catch (error) {
-                if (!f) {
-                    console.error(error.message);
-                } else {
-                    f(error.message);
-                }
-            }
-        },
-
-        showDialog: function (options, s, f) {
-
-            if (!options.name) {
-                options.name = "";
-            }
-            if (!options.message) {
-                options.message = "";
-            }
-            if (!options.caption) {
-                options.caption = "";
-            }
-            if (!options.description) {
-                options.description = "";
-            }
-            if (!options.href) {
-                options.href = "";
-            }
-            if (!options.picture) {
-                options.picture = "";
-            }
-            
-            // Try will catch errors when SDK has not been init
-            try {
-                FB.ui(options,
-                function (response) {
-                    if (response && (response.request || !response.error_code)) {
-                        s(response);
-                    } else {
-                        f(response);
-                    }
-                });
-            } catch (error) {
-                if (!f) {
-                    console.error(error.message);
-                } else {
-                    f(error.message);
-                }
-            }
-        },
-        // Attach this to a UI element, this requires user interaction.
-        login: function (permissions, s, f) {
-            // JS SDK takes an object here but the native SDKs use array.
-            var permissionObj = {};
-            if (permissions && permissions.length > 0) {
-                permissionObj.scope = permissions.toString();
-            }
-            
-            FB.login(function (response) {
-                if (response.authResponse) {
-                    s(response);
-                } else {
-                    f(response.status);
-                }
-            }, permissionObj);
-        },
-
-        getAccessToken: function (s, f) {
-            var response = FB.getAccessToken();
-            if (!response) {
-                if (!f) {
-                    console.error("NO_TOKEN");
-                } else {
-                    f("NO_TOKEN");
-                }
-            } else {
-                s(response);
-            }
-        },
-
         logEvent: function (eventName, params, valueToSum, s, f) {
             // AppEvents are not avaliable in JS.
             s();
-        },
-
-        logPurchase: function (value, currency, s, f) {
-            // AppEvents are not avaliable in JS.
-            s();
-        },
-
-        logout: function (s, f) {
-            // Try will catch errors when SDK has not been init
-            try {
-                FB.logout( function (response) {
-                    s(response);
-                });
-            } catch (error) {
-                if (!f) {
-                    console.error(error.message);
-                } else {
-                    f(error.message);
-                }
-            }
-        },
-
-        api: function (graphPath, permissions, s, f) {
-            // JS API does not take additional permissions
-            
-            // Try will catch errors when SDK has not been init
-            try {
-                FB.api(graphPath, function (response) {
-                    if (response.error) {
-                        f(response);
-                    } else {
-                        s(response);
-                    }
-                });
-            } catch (error) {
-                if (!f) {
-                    console.error(error.message);
-                } else {
-                    f(error.message);
-                }
-            }
         },
 
         // Browser wrapper API ONLY
@@ -182,18 +57,6 @@ if (cordova.platformId == "browser") {
 
     var facebookConnectPlugin = {
 
-        getLoginStatus: function (s, f) {
-            exec(s, f, "FacebookConnectPlugin", "getLoginStatus", []);
-        },
-
-        showDialog: function (options, s, f) {
-            exec(s, f, "FacebookConnectPlugin", "showDialog", [options]);
-        },
-
-        login: function (permissions, s, f) {
-            exec(s, f, "FacebookConnectPlugin", "login", permissions);
-        },
-
         logEvent: function(name, params, valueToSum, s, f) {
             // Prevent NSNulls getting into iOS, messes up our [command.argument count]
             if (!params && !valueToSum) {
@@ -205,23 +68,6 @@ if (cordova.platformId == "browser") {
             } else {
                 f("Invalid arguments");
             }
-        },
-
-        logPurchase: function(value, currency, s, f) {
-            exec(s, f, "FacebookConnectPlugin", "logPurchase", [value, currency]);
-        },
-
-        getAccessToken: function(s, f) {
-            exec(s, f, "FacebookConnectPlugin", "getAccessToken", []);
-        },
-
-        logout: function (s, f) {
-            exec(s, f, "FacebookConnectPlugin", "logout", []);
-        },
-
-        api: function (graphPath, permissions, s, f) {
-            if (!permissions) { permissions = []; }
-            exec(s, f, "FacebookConnectPlugin", "graphApi", [graphPath, permissions]);
         }
     };
 
